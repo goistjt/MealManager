dccc1b6aa617876630c38dfda6b7dddbc659e212
goistjt <goistjt@rose-hulman.edu>
2016-04-21 11:02:05 -0400

    Initial Project Commit

---
+++ app/src/main/java/com/csse333/mealmanager/TaskSelectActivity.java
@@ -0,0 +1,13 @@
+package com.csse333.mealmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_select);
+    }
+}
